1a870f85ad5ca714c192eb21a75c72436753c69e
gazoil2 <bverdun@alumno.huergo.edu.ar>
2024-04-14 23:33:40 -0300

    Normalized the force
    Me and my homies HATE NORMALIZING THE FORCE

--- scripts/CueComponent.gd
+++ scripts/CueComponent.gd
@@ -1,12 +1,22 @@
 extends Node
 class_name CueObject
 
-var action : bool
-var ballPosition : Vector3
-var mousePosition : Vector3
+
 var cameraNode : Camera3D
 var cueBall : RigidBody3D
-# Called when the node enters the scene tree for the first time.
+
+var action : bool
+
+var ballPosition : Vector3
+var mousePosition : Vector3
+var posDifference : Vector3
+
+var appliedForce : Vector3
+var forceMultiplier : float = 200
+var maxForce : float = 200
+var xForce : float
+var zForce : float
+
 func _ready():
 	cameraNode = get_node("../Camera3D")
 	cueBall = get_node("../PoolTable/CueBall")
@@ -15,26 +25,32 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	var progressbar : ProgressBar = get_node("ProgressBar")
+	mousePosition = cameraNode.project_position(get_viewport().get_mouse_position(),1)
+	if action == true: 
+		appliedForce = getStrokePower(ballPosition,mousePosition)
+		var percentage = appliedForce.length() / 2
+		progressbar.set_value_no_signal(percentage)
 	if Input.is_action_just_released("LeftClick") and action == true:
 		action = false
-		getStrokePower(ballPosition,cameraNode.project_position(get_viewport().get_mouse_position(),1))
+		applyStrokePower()
 	pass
 
-
+func applyStrokePower():
+	cueBall.apply_force(appliedForce, Vector3.ZERO)
+	
 func getStrokePower(ballPos: Vector3,mousePos: Vector3):
-	var posDifference = ballPos - mousePos
-	var forceMultiplier = 300
-	var maxForce = 200
-	var xForce = forceMultiplier * posDifference[0]
-	var zForce = forceMultiplier * posDifference[2]
-	if xForce >= maxForce: xForce = maxForce 
-	if zForce >= maxForce: zForce = maxForce
-	if zForce < 0 and zForce <= maxForce * -1: zForce = maxForce * -1
-	if xForce < 0 and xForce <= maxForce * -1: xForce = maxForce * -1
+	posDifference = ballPos - mousePos
 	posDifference[1] = 0
-	cueBall.apply_force(Vector3(xForce,0,zForce),posDifference)
-	pass
-
+	posDifference.normalized()
+	xForce =  posDifference[0] * forceMultiplier
+	zForce =  posDifference[2] * forceMultiplier
+	if xForce >= maxForce: xForce = maxForce 
+	if xForce <= -maxForce: xForce = -maxForce
+	if zForce >= maxForce: zForce = maxForce
+	if zForce <= -maxForce: zForce = -maxForce
+	
+	return Vector3(xForce,0,zForce) 
 
 func _on_cue_ball_input_event(_camera, _event, position, _normal, _shape_idx):
 	if !Input.is_action_just_pressed("LeftClick"): return
