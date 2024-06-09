c96d6e90d652b1ee8271eeff5effb9c3ff437620
Bruno Malvasio <malvasiobrunoenzo@gmail.com>
2024-06-09 00:54:20 -0300

    added rotating camera in main menu

--- scripts/mainmenus.gd
+++ scripts/mainmenus.gd
@@ -1,9 +1,15 @@
 extends CanvasLayer
 
 @onready var luz = $fondo/SpotLight3D
+@onready var camara_rot = $fondo/SubViewportContainer/SubViewport/Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	while true:
+		await get_tree().create_timer(0.02).timeout
+		camara_rot.rotate_y(deg_to_rad(1))
+
+func _ready_2():
 	while true:
 		luz.light_energy = 16
 		await get_tree().create_timer(randf_range(0.5,3)).timeout
